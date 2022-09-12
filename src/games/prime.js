274e770a74f8e73bd47938c1f11664177933fb53
sulianova <ulianova.sofia@gmail.com>
2022-09-12 11:15:38 +0300

    Fix lint for brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,9 +4,9 @@ import { getRandomInt, isPrime } from '../functions.js';
 const explanation = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getQA = () => {
-    const question = getRandomInt(100);
-    const correct = (isPrime(question)) ? 'yes' : 'no';
-    return [question, correct];
-  };
-  
-  export default () => run(explanation, getQA);
+  const question = getRandomInt(100);
+  const correct = (isPrime(question)) ? 'yes' : 'no';
+  return [question, correct];
+};
+
+export default () => run(explanation, getQA);
