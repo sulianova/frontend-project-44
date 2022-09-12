80ab4bc43e07ce828cfb9a420a80904771b04a94
sulianova <ulianova.sofia@gmail.com>
2022-09-12 11:10:07 +0300

    Add brain-prime

--- src/functions.js
+++ src/functions.js
@@ -12,3 +12,14 @@ export const getProgression = (fn, start, length) => {
   const arrayForProgression = new Array(length).fill(0);
   return arrayForProgression.map((element, index) => fn(start + index));
 };
+
+export const isPrime = (num) => {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
---
+++ src/games/prime.js
@@ -0,0 +1,12 @@
+import { run } from '../index.js';
+import { getRandomInt, isPrime } from '../functions.js';
+
+const explanation = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getQA = () => {
+    const question = getRandomInt(100);
+    const correct = (isPrime(question)) ? 'yes' : 'no';
+    return [question, correct];
+  };
+  
+  export default () => run(explanation, getQA);
